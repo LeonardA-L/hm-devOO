b61ffa31c1b9a3a74ce5ad471ba63f776ab1bad4
Quentin Lemaire <quentyn.lemaire@gmail.com>
2014-10-22 11:45:48 +0200

    OHHHHH

--- titan-shippment/src/main/Main.java
+++ titan-shippment/src/main/Main.java
@@ -9,5 +9,7 @@ public class Main {
 		System.out.println("ZBRAAAAAA");
 		System.out.println("ZBRIIIIII");
 		System.out.println("Oh ze Trouve le TITAN du jour?");
+		System.out.println("OHHHHHH CHOCO");
+		System.out.println("Il faut 3 mecs à plein temps !");
 	}
 }
