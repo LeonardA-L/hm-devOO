29befec28a3783748cd260890dca2f539111a7f7
lallainlau <lallainlau@if501-213-09.insa-lyon.fr>
2014-11-03 10:13:33 +0100

    Adding choco-compliant graph, and tests for it

---
+++ titan-shippment/src/tests/PlanTest.java
@@ -0,0 +1,79 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import model.agglomeration.Noeud;
+import model.agglomeration.Plan;
+import model.agglomeration.Troncon;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PlanTest {
+
+	Plan dummy;
+	
+	public static Plan DummyPlanCreate(int nbNodes, int maxX, int maxY){
+		// creating map
+		String[] rues = {"Avenue des titans", "Rue de Léonard", "wess c ma ru, ru de yaannnn", "Anton Long Avenue", "GIGA AVENUE", "SSSSSSSS-GBD street"};
+		
+		Noeud entrepot = new Noeud(20,30,-1, new ArrayList<Troncon>());
+		Plan plan = new Plan(entrepot, new ArrayList<Noeud>());
+		
+		// generate random nodes
+		for (int i = 0; i < nbNodes; ++i) {
+			int x = (int)Math.floor(Math.random()*maxX);
+			int y = (int)Math.floor(Math.random()*maxY);
+			int id = i;
+			Noeud noeud = new Noeud(x, y, id, new ArrayList<Troncon>());
+			plan.addNoeud(noeud);
+		}
+		
+		// generate random troncons
+		int nbTroncons = 10;
+		for (int i = 0; i < nbTroncons; ++i) {
+			int nodeInIndex = (int)Math.floor(Math.random()*nbNodes);
+			int nodeOutIndex = (int)Math.floor(Math.random()*nbNodes);
+			
+			// no boucle
+			if (nodeInIndex == nodeOutIndex) {
+				continue;
+			}
+			
+			Noeud nodeIn = plan.getNoeudById(nodeInIndex);
+			Noeud nodeOut = plan.getNoeudById(nodeOutIndex);
+			
+			int vitesse = (int)Math.floor(Math.random()*50);
+			int longueur = (int)Math.floor(Math.random()*30) +1;
+			String rue = rues[(int)Math.floor(Math.random()*rues.length)];
+			
+			double doubleSens = Math.random();
+			
+			Troncon troncon1 = new Troncon(rue, vitesse, longueur, nodeOut);
+			nodeIn.addTroncon(troncon1);
+			
+			if (doubleSens < 0.5) {
+				Troncon troncon2 = new Troncon(rue, vitesse, longueur, nodeIn);
+				nodeOut.addTroncon(troncon2);
+			}			
+		}
+		return plan;
+	}
+
+	@Before
+	public void setUp() {
+		int nbNodes = 20;
+		int maxX = 100;
+		int maxY = 100;
+		dummy = DummyPlanCreate(nbNodes, maxX, maxY);
+	}
+	
+	
+	@Test
+	public void weHaveAPlan() {
+		assertNotNull(this.dummy);
+	}
+
+}
---
+++ titan-shippment/src/utils/ShippmentGraph.java
@@ -0,0 +1,96 @@
+package utils;
+import java.util.ArrayList;
+
+import model.agglomeration.Noeud;
+import model.agglomeration.Troncon;
+
+/**
+ * @author Christine Solnon
+ *
+ */
+
+public class ShippmentGraph implements Graph {
+	
+	private final int NO_COST = 0;
+	
+	private int nbVertices;
+	private int maxArcCost;
+	private int minArcCost;
+	private int[][] cost;
+	private ArrayList<ArrayList<Integer>> succ; 
+
+	/**
+	 * Creates a graph such that each vertex is connected to the next <code>d</code> vertices (modulo <code>n</code>) and
+	 * such that the cost of each arc is a randomly chosen integer ranging between <code>min</code> and <code>max</code>
+	 * @param n a number of vertices such that <code>n > 0</code>
+	 */
+	public ShippmentGraph(int n){
+		nbVertices = n;
+		maxArcCost = -1;
+		minArcCost = Integer.MAX_VALUE;
+		cost = new int[nbVertices][nbVertices];
+		succ = new ArrayList<ArrayList<Integer>>(n);
+		while (succ.size() < n) succ.add(null); 
+	}
+
+	public int getMaxArcCost() {
+		return maxArcCost;
+	}
+
+	public int getMinArcCost() {
+		return minArcCost;
+	}
+
+	public int getNbVertices() {
+		return nbVertices;
+	}
+
+	public int[][] getCost(){
+		return cost;
+	}
+
+	public int[] getSucc(int i) throws ArrayIndexOutOfBoundsException{
+		if ((i<0) || (i>=nbVertices))
+			throw new ArrayIndexOutOfBoundsException();
+		int[] tab = new int[succ.get(i).size()];
+		for(int j=0;j<tab.length;j++){
+			tab[j] = succ.get(i).get(j);
+		}
+		return tab;
+	}
+
+
+	public int getNbSucc(int i) throws ArrayIndexOutOfBoundsException {
+		if ((i<0) || (i>=nbVertices))
+			throw new ArrayIndexOutOfBoundsException();
+		return succ.get(i).size();
+	}
+
+	public void addNode(Noeud n){
+		ArrayList<Integer> exitNodesIds = new ArrayList<Integer>();
+		for(Troncon t : n.getTroncons()){
+			exitNodesIds.add(t.getNoeudDestination().getId());
+			int arcCost = (int)(((double)t.getLongueur()) / t.getVitesse());
+			cost[n.getId()][t.getNoeudDestination().getId()] = arcCost;
+			updateMinMax(arcCost);
+		}
+		succ.set(n.getId(), exitNodesIds);
+	}
+
+	private void updateMinMax(int arcCost) {
+		if(arcCost > maxArcCost)
+			maxArcCost = arcCost;
+		if(arcCost < minArcCost)
+			minArcCost = arcCost;
+	}
+	
+	public void fillBlankCosts(){
+		for (int i = 0; i < cost.length; i++) {
+			for (int j = 0; j < cost.length; j++) {
+				if(cost[i][j] == NO_COST)
+					cost[i][j] = maxArcCost+1;
+			}
+		}
+	}
+
+}
