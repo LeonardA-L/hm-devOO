91344375dba7e6af352075f511692bdcb6ba7347
mthoretton <mathieu@thoretton.com>
2014-11-03 11:20:25 +0100

    Adding classes
    Adding classes

---
+++ titan-shippment/src/model/agglomeration/InterfacingAggloWithUtils.java
@@ -0,0 +1,11 @@
+package model.agglomeration;
+
+public class InterfacingAggloWithUtils {
+	public boolean GetAgglosFromBuilder(String absFilePath) {
+		return true;
+	}
+	
+	public boolean GeneratePlan() {
+		return true;
+	}
+}
---
+++ titan-shippment/src/model/planning/InterfacingPlanningWithUtils.java
@@ -0,0 +1,11 @@
+package model.planning;
+
+public class InterfacingPlanningWithUtils {
+	public boolean GetPlanningsFromBuilder(String absFilePath) {
+		return true;
+	}
+	
+	public boolean GenerateLivraisons () {
+		return true;
+	}
+}
