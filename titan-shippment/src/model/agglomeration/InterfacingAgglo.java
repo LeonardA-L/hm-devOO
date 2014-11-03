19d420f913fef14ecc571607d6b75d04bce8c800
julien-monniot <n.ceska@gmail.com>
2014-11-03 11:36:21 +0100

    Refactoring interfaces

---
+++ titan-shippment/src/model/agglomeration/InterfacingAgglo.java
@@ -0,0 +1,11 @@
+package model.agglomeration;
+
+public class InterfacingAgglo {
+	public boolean GetAgglosFromBuilder(String absFilePath) {
+		return true;
+	}
+	
+	public boolean GeneratePlan() {
+		return true;
+	}
+}
