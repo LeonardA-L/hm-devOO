a649516d3d4c1f4c615b83955e3d846d1b5b34d4
julien-monniot <n.ceska@gmail.com>
2014-11-05 09:26:34 +0100

    Small Change in diagram

--- titan-shippment/src/model/planning/InterfacePlanning.java
+++ titan-shippment/src/model/planning/InterfacePlanning.java
@@ -1,11 +1,27 @@
 package model.planning;
 
+import utils.XMLBuilder;
+
+
 public class InterfacePlanning {
+	
+	
+	
+	/*	Call a method in XMLBuilder, giving it the name of the file
+	 * 	containing the plan. Then get back the array of elements to 
+	 *  be used and build all livraisons from it 
+	 * 	@param absFilePath	path to the xml File
+	 *  @return true or false depending on the success of method (i.d. 
+	 *  all livraisons have been created successfully)
+	 */
 	public boolean GetPlanningsFromBuilder(String absFilePath) {
+		
 		return true;
 	}
 	
-	public boolean GenerateLivraisons () {
+	
+	
+	private boolean generateLivraisons () {
 		return true;
 	}
 }
