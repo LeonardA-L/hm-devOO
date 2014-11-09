4774008988a02f1232e6e986e730f80fe03e3c6f
LeonardA-L <leonard@allain-launay.fr>
2014-11-09 18:36:32 +0100

    Adds a method that computes a PlageHoraire object into an array of boundaries displayed as int TimeStamps

--- titan-shippment/src/model/planning/PlageHoraire.java
+++ titan-shippment/src/model/planning/PlageHoraire.java
@@ -15,6 +15,33 @@ public class PlageHoraire {
 		this.heureFin = heureFin;
 	}
 
+	/**
+	 * Parses the String dates of the object into boundary timestamps
+	 * @return
+	 */
+	public int[] getBounds(){
+		int[] bounds = new int[2];
+		String[] startArray = heureDebut.split(":");
+		// Her Majesty's Service
+		
+		int h = Integer.parseInt(startArray[0]);
+		int m = Integer.parseInt(startArray[1]);
+		int s = Integer.parseInt(startArray[2]);
+		m*=60;
+		h*=60*60;
+		bounds[0] = h+m+s;
+		
+		String[] endArray = heureFin.split(":");
+		h = Integer.parseInt(endArray[0]);
+		m = Integer.parseInt(endArray[1]);
+		s = Integer.parseInt(endArray[2]);
+		m*=60;
+		h*=60*60;
+		bounds[1] = h+m+s;
+		
+		return bounds;
+	}
+
 	public String getHeureDebut() {
 		return heureDebut;
 	}
