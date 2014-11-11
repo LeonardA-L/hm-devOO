bff15bec2854616e650dd2fd4d93822d5b80d158
lcapatina <lau.capatina@yahoo.com>
2014-11-11 22:10:22 +0100

    Petit bug dans controller

--- titan-shippment/src/controller/Controller.java
+++ titan-shippment/src/controller/Controller.java
@@ -315,7 +315,6 @@ public class Controller implements ActionListener {
 			boolean suppr = interfaceView.confirmUserInput("Suppression", "Supprimer cette livraison ? ");
 			if (suppr) {
 				undoRedo.InsertRemoveCmd(idNoeud);
-				//interfaceView.removeShippment(idNoeud);
 				interfaceView.getVuePanel().getVue_tournee().setTournee(interfacePlanning.getTournee());
 				interfaceView.repaint();
 				return;
